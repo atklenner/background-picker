e49c6676eabc218c15118fdc9b8a116b0e2e6de0
Andrew Klenner <atklenner@gmail.com>
2023-03-13 12:12:42 -0700

    add custom color choice and contrasting text

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ document.getElementById("blue").addEventListener("click", () => {
   body.style.backgroundColor = "rgb(0, 254, 255)";
   body.style.color = "white";
 });
+
+document.getElementById("picker").addEventListener("change", (e) => {
+  const color = e.target.value;
+  body.style.backgroundColor = color;
+
+  const red = parseInt(color.slice(1, 3), 16);
+  const green = parseInt(color.slice(3, 5), 16);
+  const blue = parseInt(color.slice(5, 7), 16);
+
+  const value = red * 0.299 + green * 0.587 + blue * 0.114;
+
+  if (value < 186) {
+    body.style.color = "white";
+  } else {
+    body.style.color = "black";
+  }
+});
