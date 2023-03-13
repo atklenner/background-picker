614b734633c15ed1b4109f3b5dea83c7712d2d57
Andrew Klenner <atklenner@gmail.com>
2023-03-13 11:53:00 -0700

    write better JS and CSS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+const body = document.querySelector("body");
 
+document.getElementById("purple").addEventListener("click", () => {
+  body.style.backgroundColor = "rgb(241, 63, 247)";
+  body.style.color = "white";
+});
 
-function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
-}
+document.getElementById("green").addEventListener("click", () => {
+  body.style.backgroundColor = "rgb(0, 253, 81)";
+  body.style.color = "white";
+});
 
-function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
-}
-
-function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'white'
-}
+document.getElementById("blue").addEventListener("click", () => {
+  body.style.backgroundColor = "rgb(0, 254, 255)";
+  body.style.color = "white";
+});
